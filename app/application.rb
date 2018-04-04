9e61cd5d5f4a5aeda6ca7b113ea85cd7525558b9
Alex McKean <amckean@me.com>
2018-04-04 14:44:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,8 +27,9 @@ class Application
       if @@items.include? item_to_add
         @@cart << item_to_add
         resp.write "added #{item_to_add}"
-      else
+      else 
         resp.write "We don't have that item!"
+      end
     else
       resp.write "Path Not Found"
     end
